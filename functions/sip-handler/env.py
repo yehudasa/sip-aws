aa12bfbb7b47dc12a2e5c63adde8eb298e7599bf
Yehuda Sadeh <yehuda@redhat.com>
2021-02-11 12:59:10 -0800

    sip-handler: adjust obj order table name
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- functions/sip-handler/env.py
+++ functions/sip-handler/env.py
@@ -8,14 +8,15 @@ logger.setLevel(logging.INFO)
 
 class Params:
    def __init__(self):
-        self.obj_order_table = os.environ.get('DYNAMODB_ORDER_TABLE') or 's3_obj_order'
+        self.db_prefix = os.environ.get('DB_PREFIX') or ''
+
+        self.obj_order_table = self.db_prefix + (os.environ.get('DYNAMODB_ORDER_TABLE') or 's3_obj_order')
 
         self.datalog_window_size = int(os.environ.get('DATALOG_WINDOW_SIZE') or '30')
         self.datalog_num_shards = int(os.environ.get('DATALOG_NUM_SHARDS') or '16')
 
         self.bilog_num_shards = int(os.environ.get('BILOG_NUM_SHARDS') or '16')
 
-        self.db_prefix = os.environ.get('DB_PREFIX') or ''
         
         
 env_params = Params()
